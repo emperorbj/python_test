6f6eee073539896af652f52525e2159a1552dd12
emperorbj <cwaku96@gmail.com>
2024-06-06 05:36:56 +0100

    file opening

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
 # testing python functions 
-names = ['prince', 'princess', 'king']
-def add_name(name):
-    names.append(name)
-    print(names)
-    for x in names:
-        print(x)
+# names = ['prince', 'princess', 'king']
+# def add_name(name):
+#     names.append(name)
+#     print(names)
+#     for x in names:
+#         print(x)
 
-add_name('queen')
+# add_name('queen')
+
+f = open("test.txt", "r")
+print(f.read())
+
+for x in f:
+    print(x)
+    
+f.close()
